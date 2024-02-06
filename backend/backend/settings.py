e4a4e266e48502cc70e43580f6236c0a690b31b2
Leo <chappuisleopold@gmail.com>
2024-02-06 19:03:24 +0100

    removed and changed secret

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -23,10 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-9naw!eym4c00o%v%)8s7$7p4tf+cadtv0(43qbed%$phb)$u@6'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['dbsapi.lchappuis.fr', 'localhost', 'dbs.etu-utc.fr']
 CSRF_TRUSTED_ORIGINS = [
